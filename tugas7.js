092bce1455d1c31d7e945465bd525dce7d7e1319
Ramdhawa <shinlia21@gmail.com>
2020-08-03 13:07:34 +0800

    add tugas 7

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+var data_tinggi=[157,180,163,154,172,171,179,183,169,167];
+console.log("Perulangan Biasa :");
+for(var i=0;i<data_tinggi.length;i++)
+{
+  console.log(data_tinggi[i]);
+}
+console.log("Perulangan For Off :");
+for(let a of data_tinggi)
+{
+  console.log(a);
+}
